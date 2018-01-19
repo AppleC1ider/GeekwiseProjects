29e28cc48cd22f3b6882d7937c42751db4dc6de9
Daniel Aubuchon <finallylegit@Daniels-MacBook-Pro.local>
2018-01-18 20:59:39 -0800

    learned about primitives and objects and variables

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+var yes = false;
+console.log(yes);
+var firstNumnber = 22;
+var secondNumber = 11;
+
+{
+let yo = "this keeps you from repeating variables and is enclosed in its scope"
+}
+
+
+{//const var
+const truth = "ep8 was .... terrible...";
+console.log(truth);
+}
+// cannot redifine or redeclare
+const truth = "ep8 was ... meh...";
+console.log(truth);
+// does not get hoisted
+
+//dates
+let rightNow = new Date();
+console.log(Date.parse(rightNow));
+//quick maths
+var iLikePie = Math.PI;
+console.log(iLikePie);
+
+
+
+var string ="lallalalala";
+
+var newString = new String("lalalalal");
+
+console.log(string);
+console.log(newString.toUpperCase());
