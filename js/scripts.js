a9ed80351a170da579dd3b997142552aeaf0e0c3
Daniel Aubuchon <finallylegit@Daniels-MacBook-Pro.local>
2018-01-23 18:59:31 -0800

    day 2 stuff

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,45 @@ var newString = new String("lalalalal");
 
 console.log(string);
 console.log(newString.toUpperCase());
+
+
+
+
+
+
+
+
+var myArray = ["Hello World", "sup dawg", "sup dog"];
+console.log(myArray.length);
+
+
+
+
+
+var myObject = {
+  myString: "Wello World",
+  magicNumber: 42
+};
+
+
+
+
+
+/*
+
+let helloMessage = alert("Hey there buy my stuff for $9.99...");
+
+let confirmMessage = confirm("Is your name Jesus?");
+console.log(confirmMessage);
+
+let promptMessage = prompt("tell me the password...");
+console.log(promptMessage);
+*/
+
+
+let firstName = prompt("What is your first name?");
+let lastName = prompt("What is your last name?");
+console.log(firstName);
+alert("Your last name is " + lastName);
+let birthday = prompt("What is your birthday?", " Please answer in the following format: 1/1/1900");
+confirm(new Date(birthday));
